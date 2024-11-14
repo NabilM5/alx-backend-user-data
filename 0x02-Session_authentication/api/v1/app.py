e03a776dd6709fb2b7938b04a9efca7a5b3744b8
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-11-14 16:38:41 +0300

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
